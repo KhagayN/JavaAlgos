d6ca530efc5d0cd25aaad1a80bce342b585c0794
khagayn <crtemois612@gmail.com>
2019-10-12 16:05:53 -0400

    Add TwoSum one pass

--- src/problems/TwoSum.java
+++ src/problems/TwoSum.java
@@ -35,11 +35,26 @@ public class TwoSum {
         throw new IllegalStateException("There are no two values in the array that sum to the target");
     }
 
+    public static int[] twoSumOnePass(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[]{i, map.get(complement)};
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        throw new IllegalStateException("There are no two values in the array that sum to the target");
+    }
+
 
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-        int[] result = twoSumTwoPass(nums, target);
+        int target = 26;
+        int[] result = twoSumOnePass(nums, target);
         for (int i: result) {
             System.out.println(i);
         }
